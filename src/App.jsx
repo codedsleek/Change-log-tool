3a2c97bd4190e767e83ceab98c47bf6a3966abb7
Codedsleek <codedsleek@gmail.com>
2025-10-09 13:03:08 +0100

    ui fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faGear, faLifeRing } from "@fortawesome/free-solid-svg-icons";
+import logo from './assets/logo.png';
 
 // Zod schema for validation
 const signinSchema = z.object({
@@ -144,7 +145,7 @@ function App() {
 
       {/*Logo and title */}
       <div className="flex items-center justify-center mb-6">
-        <img src= "../public/logo.png" alt="Khronicle Logo" className="w-10 h-10 mr-2" />
+        <img src= {logo} alt="Khronicle Logo" className="w-10 h-10 mr-2" />
         <h1 className="text-3xl text-amber-800">Khronicle</h1>
       </div>
 
